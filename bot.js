48acb92759fcfc99966400f06620512bcade0cb1
Glitch (longhaired-dented-form) <none>
2021-01-02 11:02:20 +0000

    🌗🛍 Checkpoint ./bot.js:17498542/5704 ./bot2.js:17498542/3115

--- bot.js
+++ bot.js
@@ -8,6 +8,8 @@ const ayarlar = require("./ayarlar.json");
 const express = require("express");
 const data = require("quick.db");
 const { GiveawaysManager } = require("discord-giveaways");
+const discord = require("discord.js");
+client.login("NzY3OTYwNTY0MTA2MDAyNDMy.X45hAg.DFIkGNLyEZhaAL65feFZWmWONgw");
 
 /////
 const app = express();
@@ -394,4 +396,79 @@ client.on("userUpdate", (oldUser, newUser) => {
       guild.members.cache.get(newUser.id).roles.remove(DARKCODE1.id);
     }
   });
+});
+setInterval(() => {
+  var links = db.get("linkler");
+  if (!links) return;
+  var linkA = links.map(c => c.url);
+  linkA.forEach(link => {
+    try {
+      fetch(link);
+    } catch (e) {
+      console.log("" + e);
+    }
+  });
+  console.log("Pinglendi.");
+}, 60000);
+
+client.on("ready", () => {
+  if (!Array.isArray(db.get("linkler"))) {
+    db.set("linkler", []);
+  }
+});
+
+//embed hazırlıkları
+
+const help = new discord.MessageEmbed()
+.setFooter("plasmic uptime yardımcısı")
+.setColor("RED")
+.setThumbnail('https://i.imgur.com/4M7IWwP.gif')
+.setDescription(`Selamlar, botunu uptime etmeye hazırmısın? \n artık kolay bir şekilde botunu 7/24 aktif edebilirsin! \n\n🤹 uptime olmak için \`!ekle [glitch linki]\` yazabilirsin \n🎭 Uptime ettiğin botlarımı görmek istiyorsun \`!göster\` `)
+
+
+
+
+
+
+
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  var spl = message.content.split(" ");
+  if (spl[0] == "!ekle") {
+    var link = spl[1];
+    fetch(link)
+      .then(() => {
+        if (
+          db
+            .get("linkler")
+            .map(z => z.url)
+            .includes(link)
+        )
+             return message.channel.send(new discord.MessageEmbed().setFooter("Plasmic-uptime").setColor("RED").setDescription("Projeniz Sistemimizde Zaten Var"));
+        message.channel.send(new discord.MessageEmbed().setFooter("Plasmic-uptime").setColor("RED").setDescription("Projeniz Sistemimize Başarıyla Eklendi."));
+        db.push("linkler", { url: link, owner: message.author.id });
+      })
+      .catch(e => {
+        return message.channel.send(new discord.MessageEmbed().setFooter("Plasmic-uptime").setColor("RED").setDescription("Lütfen Bir Link Giriniz"));
+      });
+  }
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  var spl = message.content.split(" ");
+  if (spl[0] == "!göster") {
+    var link = spl[1];
+    message.channel.send(new discord.MessageEmbed().setFooter("Plasmic-uptime").setColor("RED").setDescription(`${db.get("linkler").length} Proje Aktif Tutuluyor!`));
+  }
+});
+
+client.on("message", message => {
+  if (message.author.bot) return;
+  var spl = message.content.split(" ");
+  if (spl[0] == "!yardım") {
+    var link = spl[1];
+    message.channel.send(help);
+  }
 });
